4c695441bdffe5b3431f40285180c002606d7243
Cartago <cartago.relaxingcup@gmail.com>
2024-01-18 22:11:35 +0100

    Solucionamos problema con localStorage en SSR y ponemos estilos basicos para incorporar el juego en la parte central

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,19 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import {
+  LocalStorageSsrHackService,
+  LocalStorageService,
+} from './shared/services/local-storage.service';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
-  ]
+    provideServerRendering(),
+    {
+      provide: LocalStorageService,
+      useClass: LocalStorageSsrHackService,
+    },
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/shared/services/local-storage.service.ts
+++ src/app/shared/services/local-storage.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
+
+export const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
+  providedIn: 'root',
+  factory: () => localStorage,
+});
+
+let localStorage: Storage;
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +14,9 @@ export class LocalStorageService {
   // ANCHOR : Properties
 
   // ANCHOR : Constructor
-  constructor() {}
+  constructor(@Inject(BROWSER_STORAGE) public storage: Storage) {
+    localStorage = this.storage;
+  }
 
   // ANCHOR : Methods
   public saveMaxPoinst(maxPoints: number | string): void {
@@ -17,5 +26,19 @@ export class LocalStorageService {
     const maxPoints = localStorage.getItem('maxPoints');
     if (maxPoints) return Number(maxPoints);
     return null;
+  }
+}
+// HACK : This is a hack to make the server side rendering work with fake localStorage
+@Injectable()
+export class LocalStorageSsrHackService extends LocalStorageService {
+  constructor() {
+    super({
+      clear: () => {},
+      getItem: (key: string) => JSON.stringify({ key }),
+      setItem: (key: string, value: string) => JSON.stringify({ [key]: value }),
+      key: (index: number) => index.toString(),
+      length: 0,
+      removeItem: (key: string) => JSON.stringify({ key }),
+    });
   }
 }
